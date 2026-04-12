b88a1d12e26369e0109f742aaa0ce96da4cef696
Salem Adams <salemadamscs@gmail.com>
2026-04-12 16:54:59 -0400

    feat: Save and restore plugin state

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,7 @@ TenBandAudioProcessorEditor::TenBandAudioProcessorEditor (TenBandAudioProcessor&
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (600, 400);
 }
 
 TenBandAudioProcessorEditor::~TenBandAudioProcessorEditor()
