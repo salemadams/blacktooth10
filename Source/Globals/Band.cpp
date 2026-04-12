27d6bba20435cbd1ac69f1fe152e5772391614c1
Salem Adams <salemadamscs@gmail.com>
2026-04-12 15:47:04 -0400

    fix: Change band one freq to match MXR

--- Source/Globals/Band.cpp
+++ Source/Globals/Band.cpp
@@ -1,7 +1,7 @@
 #include "Band.h"
 
-extern const Band bandOne{"BandOne", "32.5", 0.72, 32.5};
-extern const Band bandTwo{"BandTwo", "62.5", 1.39, 62.5};
+extern const Band bandOne{"BandOne", "31.25", 1.41, 31.25};
+extern const Band bandTwo{"BandTwo", "62.5", 1.41, 62.5};
 extern const Band bandThree{"BandThree", "125", 1.41, 125};
 extern const Band bandFour{"BandFour", "250", 1.41, 250};
 extern const Band bandFive{"BandFive", "500", 1.41, 500};
